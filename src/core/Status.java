2a58c6b8d016771a3a02b7c43eb54eb458d5b0ac
martintc <33560895+martintc@users.noreply.github.com>
2020-04-19 20:36:25 -0700

    Create Status.java
    Initial creation of enumeration

---
+++ src/core/Status.java
@@ -0,0 +1,11 @@
+/**
+* Enumeration that represents the current state of an individual cell on the gameboard.
+*
+* @author Todd Martin
+* @version 1.0
+*/
+
+public enum Status [
+  ALIVE,
+  DEAD;
+}
