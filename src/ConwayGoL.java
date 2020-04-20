54822f47b36868afa8741868f78ecc33dd952fbf
martintc <33560895+martintc@users.noreply.github.com>
2020-04-19 19:42:02 -0700

    Update ConwayGoL.java
    Commented with javadoc comments for class and main method.

--- src/ConwayGoL.java
+++ src/ConwayGoL.java
@@ -1,8 +1,18 @@
-
+/**
+* Entry class for Conway's Game of Life program. The program provides a main class that starts the program.
+*
+* @author Todd Martin
+* @version 1.0
+*/
 
 public class ConwayGoL 
 {
 
+  /**
+  * Main method, entry point of program.
+  *
+  * @param args Command line arguments passed into program
+  */
   public static void main (String[] args) 
   {
   
